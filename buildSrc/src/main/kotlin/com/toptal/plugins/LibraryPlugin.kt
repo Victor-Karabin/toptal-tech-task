931b86fe3b66ceb05939971ef8d04cc7ad5a0936
Viktar Karabin <viktor.karabin@gmail.com>
2024-08-23 12:05:58 +0200

    template and requirements have been added

---
+++ buildSrc/src/main/kotlin/com/toptal/plugins/LibraryPlugin.kt
@@ -0,0 +1,24 @@
+package com.toptal.plugins
+
+import com.android.build.gradle.LibraryExtension
+import com.toptal.plugins.internal.configureToolchainsAndJavaTarget
+import com.toptal.plugins.internal.getVersionCatalogVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LibraryPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) = with(project) {
+        pluginManager.apply("com.android.library")
+        pluginManager.apply("org.jetbrains.kotlin.android")
+
+        extensions.getByType(LibraryExtension::class.java).apply {
+            compileSdk = getVersionCatalogVersion("build-android-compile-sdk")
+            defaultConfig.minSdk = getVersionCatalogVersion("build-android-min-sdk")
+
+            testOptions.targetSdk = getVersionCatalogVersion("build-android-target-sdk")
+        }
+
+        configureToolchainsAndJavaTarget()
+    }
+}
